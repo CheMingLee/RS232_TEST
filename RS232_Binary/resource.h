b87b6ea8b3679ad03436d4c649be6c5df4ff937d
CheMingLee <jmli1993@gmail.com>
2021-04-09 15:47:02 +0800

    0409 update resource.h

--- RS232_Binary/resource.h
+++ RS232_Binary/resource.h
@@ -12,12 +12,13 @@
 #define IDC_BUTTON_W_OFFLINE            1002
 #define IDC_STATIC_R                    1003
 #define IDC_BUTTON_SET_COM              1004
+#define IDC_STATIC_STATUS               1006
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
